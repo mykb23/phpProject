8167df94da5284383707fcebde7850106d052d07
Alabi A <alabi10@yahoo.com>
2019-03-25 13:10:31 +0100

    added access control feature to the project

--- functions.php
+++ functions.php
@@ -4,3 +4,22 @@
 		header("Location: error.php");
 		exit;
 	}
+
+	function accessControl($right, $con){
+		$user = $_SESSION['login'];
+	
+		$query = "SELECT * FROM login WHERE id = '{$user['id']}'";
+		$result = mysqli_query($con, $query);
+		if($result){
+			$row = mysqli_fetch_all($result, MYSQLI_ASSOC);
+			$authority = $row[0]['authority'];
+		}
+		else {
+			errorMove("D3 ".mysqli_error($con)." in file ".__FILE__." at line ".__LINE__);
+		}
+		
+		if($authority != $right){
+			header("location: home.php");
+			exit;
+		}
+	}
